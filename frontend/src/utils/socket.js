a2940bc18574a17d9b9db2a3ae1dde24596e00a1
Priyanshu Singh Rajawat <priyanshusingh00004@gmail.com>
2025-07-07 17:20:13 +0530

    Update socket.js

--- frontend/src/utils/socket.js
+++ frontend/src/utils/socket.js
@@ -1,4 +1,4 @@
 import { io } from 'socket.io-client';
 
-const URL = 'http://localhost:5000'; // Backend server URL
+const URL = 'http://localhost:5000' || 'https://real-time-collaborative-to-do-board-npp5.onrender.com'; // Backend server URL
 export const socket = io(URL, { autoConnect: false });
